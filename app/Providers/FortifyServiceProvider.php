24f929732a3768f0229f29f1f7c31d8cbf516dd8
Luiz Eduardo <luiztsilva@protonmail.com>
2026-06-22 17:34:17 -0300

    correção fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,33 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Fortify::loginView(function () {
+            return inertia('auth/Login');
+        });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return inertia('auth/ForgotPassword');
+        });
+
+        Fortify::resetPasswordView(function (Request $request) {
+            return inertia('auth/ResetPassword', [
+                'token' => $request->token,
+                'email' => $request->email,
+            ]);
+        });
+
+        Fortify::confirmPasswordView(function () {
+            return inertia('auth/ConfirmPassword');
+        });
+
+        Fortify::twoFactorChallengeView(function () {
+            return inertia('auth/TwoFactorChallenge');
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->email;
+
+            return Limit::perMinute(5)->by($email . $request->ip());
+        });
     }
 }
